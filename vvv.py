6de71732aa28b1a70b97681c4972d3d42c2e6d80
davex-ai <omotoyedave4@gmail.com>
2026-03-10 22:17:08 +0100

    ....

--- vvv.py
+++ vvv.py
@@ -1,5 +1,7 @@
 import random
 
+import numpy as np
+
 heads = 0
 trials = 1000
 
@@ -7,5 +9,8 @@ for _ in range(trials):
     if random.random() < 0.5:
         heads += 1
 
-print(heads / trials)
-print(heads  )
+# print(heads / trials)
+# print(heads  )
+X = np.random.randn(32, 10)
+print(X)
+# s2e16
