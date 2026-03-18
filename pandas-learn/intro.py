3404ec44575a8d177bf5b1b9dde698d43989b407
davex-ai <omotoyedave4@gmail.com>
2026-03-18 14:22:38 +0100

    ....

--- pandas-learn/intro.py
+++ pandas-learn/intro.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 
 data = {
@@ -7,4 +8,25 @@ data = {
 }
 
 df = pd.DataFrame(data)
-print(df)
+# print(df)
+
+# print(df.head())      # first 5 rows
+# print(df.tail())      # last 5 rows
+# print(df.shape)       # (rows, columns)
+# print(df.columns)     # column names
+# print(df.info())      # data types and nulls
+# print(df.describe())  # summary statistics
+# print(df["name"].describe( ))  # summary statistics
+# print(df["name"])
+# print(df[["name", "score"]])
+# print(df.iloc[0])      # first row by position
+# print(df.iloc[0:2])
+# print(df[df["age"] > 16])
+# print(df[df["score"] >= 88])
+# df["passed"] = df["score"] >= 50
+# print(df)
+# print(df["score"].mean())
+# print(df["score"].max())
+# print(df["age"].min())
+
+
