25f53968f6f6a7dceafcf1c850f866893fd8a180
davex-ai <omotoyedave4@gmail.com>
2026-03-22 22:21:34 +0100

    ....

--- pandas-learn/student-performance_analysis.py
+++ pandas-learn/student-performance_analysis.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 data = {
@@ -12,5 +13,29 @@ df = pd.DataFrame(data)
 # print(df.head())
 # print(df.info())
 # print(df.describe())
-avg_score = df['math_score'] + df['english_score']
-print(avg_score.mean())
+df['avg_score'] = (df['math_score'] + df['english_score']) / 2
+print(df['avg_score'].mean())
+passed = df['avg_score'] >= 50
+high_attendance = df['attendance'] >= 90
+
+high_performers = df[(df['attendance'] >= 90) & (df['avg_score'] > 85)]
+struggling_student = df[df['attendance'] < 70]
+# print(struggling_student)
+top_3_high_performers = df.nlargest(3, 'avg_score')
+# print(top_3_high_performers)
+df['attendance_group'] = np.where(df['attendance'] >= 90, 'High', 'Low')
+
+comparison = df.groupby('attendance_group')['avg_score'].mean()
+
+df['overall_performance'] = (df['avg_score'] * 0.5) + (df['attendance'] * 0.5)
+best_overall = df.nlargest(1, 'overall_performance')
+# print(best_overall)
+
+# print(df.nlargest(1, 'attendance'))
+# print(df.nlargest(1, 'avg_score'))
+
+
+
+# print(comparison)
+# print(df.sort_values('avg_score', ascending=False))
+print(df.sort_values('attendance', ascending=False))
