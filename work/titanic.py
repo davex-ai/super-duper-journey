0ec499408b7bba3fa53695901c8ee3babc91c845
davex-ai <omotoyedave4@gmail.com>
2026-03-23 03:21:11 +0100

    training & pred

--- work/titanic.py
+++ work/titanic.py
@@ -1,8 +1,29 @@
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
 # print(df.head())
+df = df.drop(columns=['PassengerId', 'Name', 'Ticket', 'Cabin'])
 print(df.columns)
 # df.info()
 # print(df.describe())
 # print(df.isnull().sum())
+df['Age'] = df['Age'].fillna(df['Age'].median())
+df['Embarked'] = df['Embarked'].fillna(df['Embarked'].mode()[0])
+df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
+df = pd.get_dummies(df, columns=['Embarked'], drop_first=True)
+
+X = df.drop('Survived', axis=1)
+y = df['Survived']
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(model.score(X_test, y_test))
+print(model.coef_)
