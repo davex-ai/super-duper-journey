d799963687d1dca50755f6cf6b3d6dbca6f02eec
davex-ai <omotoyedave4@gmail.com>
2026-03-23 02:43:18 +0100

    inspection

--- work/titanic.py
+++ work/titanic.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
-df.head()
-df.info()
-df.describe()
-df.isnull().sum()
+# print(df.head())
+print(df.columns)
+# df.info()
+# print(df.describe())
+# print(df.isnull().sum())
